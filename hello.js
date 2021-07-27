65fd2d4a6f350bd2f0ea5580bb9b0f8a5da06304
ASIB4732 <asiburrahman444@gmail.com>
2021-07-27 23:26:47 +0600

    first day

--- hello.js
+++ hello.js
@@ -2,3 +2,33 @@ console.log(253453404);
 
 var MyMother ="Alyea Begum";
 console.log(MyMother)
+
+var name ="asibur rahman"
+var number = 1000
+console.log(name)
+console.log(number)
+console.log(typeof name)
+console.log(typeof number) 
+
+var is =true;
+var it =false;
+console.log(is, it)
+ var promise ="I promise i well become a programmer "
+
+ console.log(promise.toUpperCase() ) 
+ console.log(promise.indexOf("a"))
+ var cappital = "Dhaka is the cappital of Bangladesh"
+ console.log (cappital.toUpperCase())
+ console.log (cappital.indexOf("Bang"))
+
+
+ var inpot1 = 20;
+ var inpot2 = "40.5";
+ inpot2 =parseFloat(inpot2)
+ console.log(inpot1+inpot2)
+
+ var plas1 = .5;
+ var plas2 = .24;
+ var add =(plas1+plas2)
+ console.log(add)
+
